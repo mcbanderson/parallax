9665bac8a33ab5c5a6e8db45652029020a8843b6
mcbanderson <maxwell.c.anderson@gmail.com>
2016-01-18 00:13:29 -0800

    Update parallax.js

--- parallax.js
+++ parallax.js
@@ -14,22 +14,13 @@
     var methods = {
         init: function(options) {
             return this.each(function() {
-                this.opts = $.extend(true, {}, $.fn.parallax.defaults, options);
+                this.opts = $.extend(true, {}, $.fn.parallax.defaults, options, $(this).data());
 
-                var xPos;
-                var yPos;
                 var that = this;
 
-                $(window).scroll([xPos, yPos], function() {
-                    if (that.opts.motion === 'linear') {
-                        xPos = methods.linear.call(that, that.opts.xStart, that.opts.xSpeed);
-                        yPos = methods.linear.call(that, that.opts.yStart, that.opts.ySpeed);
-                    }
-
-                    if (that.opts.motion === 'exponential') {
-                        xPos = methods.exponential.call(that, that.opts.xStart, that.opts.xSpeed, that.opts.exponent);
-                        yPos = methods.exponential.call(that, that.opts.yStart, that.opts.ySpeed, that.opts.exponent);
-                    }
+                $(window).on("scroll load", function() {
+                    var xPos = methods.position.call(that, that.opts.xStart, that.opts.xSpeed, that.opts.exponent);
+                    var yPos = methods.position.call(that, that.opts.yStart, that.opts.ySpeed, that.opts.exponent);
 
                     methods.parallax.call(this, that, xPos, yPos, that.opts.xUnit, that.opts.yUnit);
                 });
@@ -38,12 +29,8 @@
             });
         },
 
-        exponential: function(start, speed, exponent) {
+        position: function(start, speed, exponent) {
             return start + Math.pow($(window).scrollTop(), exponent) * speed;
-        },
-
-        linear: function(start, speed) {
-            return start + $(window).scrollTop() * speed;
         },
 
         parallax: function(that, xPos, yPos, xUnit, yUnit) {
@@ -66,11 +53,10 @@
     };
 
     $.fn.parallax.defaults = {
-        exponent    : 1.25,
-        motion      : 'linear',
+        exponent    : 1,
         xSpeed      : 0,
         xStart      : 0,
-        xUnit       : '% ',
+        xUnit       : 'px ',
         ySpeed      : 1,
         yStart      : 0,
         yUnit       : 'px',
